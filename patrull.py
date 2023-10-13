ad385bdfc2cd7e9b25cc981050af77ccbe83b0c9
Märten Mikk <mm2u20@soton.ac.uk>
2023-10-13 09:33:20 +0300

    first impl of (i,j,k)

--- patrull.py
+++ patrull.py
@@ -37,8 +37,9 @@ costs = [
 
 def calculate_one_set(costs, day_number):
 
-    num_workers = len(costs)
-    num_tasks = len(costs[0])
+    number_of_soldiers = len(costs)
+    number_of_shifts = len(costs[0])
+    number_of_tasks = len(costs[0][0])
 
     # Solver
     # Create the mip solver with the SCIP backend.
@@ -48,27 +49,38 @@ def calculate_one_set(costs, day_number):
         return
 
     # Variables
-    # x[i, j] is an array of 0-1 variables, which will be 1
-    # if worker i is assigned to task j.
+    # x[i, j, k] is an array of 0-1 variables, which will be 1
+    # if soldier i is assigned to shift j and task k.
     x = {}
-    for i in range(num_workers):
-        for j in range(num_tasks):
-            x[i, j] = solver.IntVar(0, 1, "")
+    for i in range(number_of_soldiers):
+        for j in range(number_of_shifts):
+            for k in range(number_of_tasks):
+                x[i, j, k] = solver.IntVar(0, 1, "")
 
     # Constraints
-    # Each worker is assigned to at most 1 task.
-    for i in range(num_workers):
-        solver.Add(solver.Sum([x[i, j] for j in range(num_tasks)]) <= 1)
+    # Each task is assigned to exactly one worker
+    for j in range(number_of_shifts):
+        for k in range(number_of_tasks):
+            solver.Add(solver.Sum([x[i, j, k] for i in range(number_of_soldiers)]) == 1)
 
-    # Each task is assigned to exactly one worker.
-    for j in range(num_tasks):
-        solver.Add(solver.Sum([x[i, j] for i in range(num_workers)]) == 1)
+    # Each worker is assigned to at most 1 task at a given shift.
+    for i in range(number_of_soldiers):
+        for j in range(number_of_shifts):
+            solver.Add(solver.Sum([x[i, j, k] for k in range(number_of_tasks)]) <= 1)
+    """
+    for i in range(number_of_soldiers):
+        res = 0
+        for k in range(number_of_tasks):
+            
+            res += solver.Sum([x[i, j, k] for k in range(number_of_shifts)])
+        solver.Add(res <= 1)"""
 
     # Objective
     objective_terms = []
-    for i in range(num_workers):
-        for j in range(num_tasks):
-            objective_terms.append(costs[i][j] * x[i, j])
+    for i in range(number_of_soldiers):
+        for j in range(number_of_shifts):
+            for k in range(number_of_tasks):
+                objective_terms.append(costs[i][j][k] * x[i, j, k])
     solver.Minimize(solver.Sum(objective_terms))
 
     # Solve
@@ -77,29 +89,26 @@ def calculate_one_set(costs, day_number):
     # Print solution.
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
         print(f"Total cost = {solver.Objective().Value()}\n")
-        for i in range(num_workers):
-            for j in range(num_tasks):
-                # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
-                if x[i, j].solution_value() > 0.5:
-                    print(f"Worker {i} assigned to task {j}." + f" Cost: {costs[i][j]}")
+        for i in range(number_of_soldiers):
+            for j in range(number_of_shifts):
+                for k in range(number_of_tasks):
+                    # Test if x[i,j,k] is 1 (with tolerance for floating point arithmetic).
+                    if x[i, j, k].solution_value() > 0.5:
+                        print(f"Worker {i} assigned during shift {j} to task {k}." + f" Cost: {costs[i][j][k]}")
     else:
         print("No solution found.")
-
-
-if __name__ == "__main__":
-    main()
 
 
 def main(no_of_days):
     # initialise 
     costs = [
-        [0,10,10,10],
-        [10,0,10,10],
-        [10,10,0,10],
-        [10,10,10,0]
+        [[0,10],[10,10],[10,10],[10,0]],
+        [[10,10],[0,10],[10,0],[10,10]],
+        [[10,10],[10,0],[0,10],[10,10]],
+        [[10,0],[10,10],[10,10],[0,10]]
     ]
     for day_number in range(no_of_days):
         costs = calculate_one_set(costs,day_number)
 
 if __name__ == "__main__":
-    main(2)
+    main(1)
